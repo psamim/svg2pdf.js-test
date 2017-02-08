8af254283e6efca9bf640ecd02f35b83d91ccea7
Samim Pezeshki <p.samim@gmail.com>
2017-02-08 13:34:56 +0330

    Initial commit

---
+++ entry.js
@@ -0,0 +1,16 @@
+document.write("It works.");
+
+jsPDF = require('jspdf-yworks');
+svg2pdf = require('svg2pdf.js');
+
+const pdf = new jsPDF('l', 'pt', [600, 800]);
+const svg = require('!file!./file.svg');
+const div = document.createElement('div');
+div.innerHTML = svg;
+const svgElemnt = div.firstChild;
+// render the svg element
+svg2pdf(svgElemnt, pdf, {
+  xOffset: 0,
+  yOffset: 0,
+  scale: 1,
+});
