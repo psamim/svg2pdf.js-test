80d4a07139f7a15d53bb607aa5e1df7d7c744d33
Samim Pezeshki <p.samim@gmail.com>
2017-02-15 13:16:29 +0330

    Update

--- entry.js
+++ entry.js
@@ -3,7 +3,7 @@ document.write("It works.");
 jsPDF = require('jspdf-yworks');
 svg2pdf = require('svg2pdf.js');
 
-const pdf = new jsPDF('l', 'pt', [600, 800]);
+const pdf = new jsPDF('p', 'pt', [795, 924]);
 const svg = require('!raw!./file.svg');
 const div = document.createElement('div');
 div.innerHTML = svg;
